bd525fbc4772fb07fe2e9b871611b800b04f9794
MateoFernandezBanchi <matefbanchi@gmail.com>
2023-11-22 13:58:01 -0500

    improve whatsappbutton

--- src/helpers/whatsappMessage.js
+++ src/helpers/whatsappMessage.js
@@ -2,7 +2,7 @@ export const whatsappMessage = (message) => {
     console.log('hello');
     if (window.innerWidth <= 768) {
       // Dispositivo móvil, abre el enlace de WhatsApp móvil
-      window.open(`https://wa.me/+5492920416145?text=${message}, '_blank`);
+      window.open(`https://wa.me/+5492920416145?text=${message}`);
     } else {
       // Escritorio, abre el enlace de WhatsApp Web
       window.open(`https://web.whatsapp.com/send?phone=+5492920416145&text=${message}`);
